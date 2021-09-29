3c04b19ee361e2d1e739876e19f598c220910e13
shushikata <shu.42111227@gmail.com>
2021-09-30 01:19:30 +0900

    setting cors

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,3 +1,12 @@
+import { useEffect } from "react";
+import { fetchRestaurants } from "../apis/restaurants";
+
 export const Restaurants = () => {
+  useEffect(() => {
+    fetchRestaurants().then((data) => {
+      console.log(data);
+    });
+  }, []);
+
   return <>レストラン一覧</>;
 };
