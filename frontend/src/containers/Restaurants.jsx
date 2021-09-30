d6bd28372abf28b89d4b3797e8d2608dee78e861
shushikata <shu.42111227@gmail.com>
2021-09-30 14:48:22 +0900

    add styled-components

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,5 +1,28 @@
 import { useEffect } from "react";
+import styled from "styled-components";
+
 import { fetchRestaurants } from "../apis/restaurants";
+
+import MainLogo from "../images/logo.png";
+import MainCoverImage from "../images/main-cover-image.png";
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 export const Restaurants = () => {
   useEffect(() => {
@@ -8,5 +31,14 @@ export const Restaurants = () => {
     });
   }, []);
 
-  return <>レストラン一覧</>;
+  return (
+    <>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo" />
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+    </>
+  );
 };
