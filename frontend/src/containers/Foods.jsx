5fb7a1d9ec00534296f3c29a905b0e6555b5943c
shushikata <shu.42111227@gmail.com>
2021-10-02 18:39:37 +0900

    styled-foods-page

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useReducer } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 import {
   initialState as foodsInitialState,
@@ -7,8 +9,44 @@ import {
 } from "../reducers/foods";
 
 import { fetchFoods } from "../apis/foods";
+import { COLORS } from "../style_constants";
+import { LocalMallIcon } from "../components/Icons";
+import { FoodWrapper } from "../components/FoodWrapper";
+import Skeleton from "@material-ui/lab/Skeleton";
 
 import { REQUEST_STATE } from "../constants";
+
+import MainLogo from "../images/logo.png";
+import FoodImage from "../images/food-image.jpg";
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 8px 32px;
+`;
+
+const BagIconWrapper = styled.div`
+  padding-top: 24px;
+`;
+
+const ColoredBagIcon = styled(LocalMallIcon)`
+  color: ${COLORS.MAIN};
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const FoodsList = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+  margin-bottom: 50px;
+`;
+
+const ItemWrapper = styled.div`
+  margin: 16px;
+`;
 
 export const Foods = (props) => {
   const restaurantsId = props.match.params.restaurantsId;
@@ -29,13 +67,39 @@ export const Foods = (props) => {
 
   return (
     <>
-      {foodsState.fetchState === REQUEST_STATE.LOADING ? (
-        <>
-          <p>ロード中...</p>
-        </>
-      ) : (
-        foodsState.foodsList.map((food) => <div key={food.id}>{food.name}</div>)
-      )}
+      <HeaderWrapper>
+        <Link to="/restaurants">
+          <MainLogoImage src={MainLogo} alt="main logo" />
+        </Link>
+        <BagIconWrapper>
+          <Link to="/orders">
+            <ColoredBagIcon fontSize="large" />
+          </Link>
+        </BagIconWrapper>
+      </HeaderWrapper>
+      <FoodsList></FoodsList>
+
+      <FoodsList>
+        {foodsState.fetchState === REQUEST_STATE.LOADING ? (
+          <>
+            {[...Array(12).keys()].map((i) => (
+              <ItemWrapper key={i}>
+                <Skeleton key={i} variant="rect" width={450} height={180} />
+              </ItemWrapper>
+            ))}
+          </>
+        ) : (
+          foodsState.foodsList.map((food) => (
+            <ItemWrapper key={food.id}>
+              <FoodWrapper
+                food={food}
+                onClickFoodWrapper={(food) => console.log(food)}
+                imageUrl={FoodImage}
+              />
+            </ItemWrapper>
+          ))
+        )}
+      </FoodsList>
     </>
   );
 };
