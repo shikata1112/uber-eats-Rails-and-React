411b4f54d99a17ebd0966b5d03386a8448455140
shushikata <shu.42111227@gmail.com>
2021-10-02 15:11:49 +0900

    fetch foods

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,10 +1,17 @@
-export const Foods = (props) => {
-  const { match } = props;
+import { useEffect } from "react";
+import { fetchFoods } from "../apis/foods";
+
+export const Foods = ({ match }) => {
+  useEffect(() => {
+    fetchFoods(match.params.restaurantsId).then((data) => {
+      console.log(data);
+    });
+  }, []);
 
   return (
     <>
       フード一覧
-      <p>restaurant-idは {match.params.restaurantsId} です</p>
+      <p>hoge</p>
     </>
   );
 };
