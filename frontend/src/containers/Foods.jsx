e8e90e45b5bf9fed6767afb569719e924333daee
shushikata <shu.42111227@gmail.com>
2021-10-02 16:36:46 +0900

    fix props

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,9 +1,10 @@
 import { useEffect } from "react";
 import { fetchFoods } from "../apis/foods";
 
-export const Foods = ({ match }) => {
+export const Foods = (props) => {
+  const restaurantsId = props.match.params.restaurantsId;
   useEffect(() => {
-    fetchFoods(match.params.restaurantsId).then((data) => {
+    fetchFoods(restaurantsId).then((data) => {
       console.log(data);
     });
   }, []);
