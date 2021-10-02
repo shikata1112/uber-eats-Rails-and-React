29fc48baee1f17c3f0d79830c55a4ef7ca71a24a
shushikata <shu.42111227@gmail.com>
2021-10-02 17:25:06 +0900

    display foods-data

--- frontend/src/containers/Foods.jsx
+++ frontend/src/containers/Foods.jsx
@@ -1,18 +1,41 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
+
+import {
+  initialState as foodsInitialState,
+  foodsActionTypes,
+  foodsReducer,
+} from "../reducers/foods";
+
 import { fetchFoods } from "../apis/foods";
+
+import { REQUEST_STATE } from "../constants";
 
 export const Foods = (props) => {
   const restaurantsId = props.match.params.restaurantsId;
+
+  const [foodsState, dispatch] = useReducer(foodsReducer, foodsInitialState);
+
   useEffect(() => {
+    dispatch({ type: foodsActionTypes.FETCHING });
     fetchFoods(restaurantsId).then((data) => {
-      console.log(data);
+      dispatch({
+        type: foodsActionTypes.FETCH_SUCCESS,
+        payload: {
+          foods: data.foods,
+        },
+      });
     });
   }, []);
 
   return (
     <>
-      フード一覧
-      <p>hoge</p>
+      {foodsState.fetchState === REQUEST_STATE.LOADING ? (
+        <>
+          <p>ロード中...</p>
+        </>
+      ) : (
+        foodsState.foodsList.map((food) => <div key={food.id}>{food.name}</div>)
+      )}
     </>
   );
 };
